1be13d48b6f5c1847514d695db4c2a9ce9936cf7
Daniel Berg <Berg.iodaniel@gmail.com>
2022-06-09 16:03:21 +0200

    changed tabs to spaces

--- modules/calender.js
+++ modules/calender.js
@@ -3,78 +3,78 @@ const calender = document.getElementById("calender");
 let selectedDate;
 let month = new Date();
 const months = [
-	"Januari",
-	"Februari",
-	"Marsh",
-	"April",
-	"Maj",
-	"Juni",
-	"Juli",
-	"Augusti",
-	"September",
-	"October",
-	"November",
-	"December",
+  "Januari",
+  "Februari",
+  "Marsh",
+  "April",
+  "Maj",
+  "Juni",
+  "Juli",
+  "Augusti",
+  "September",
+  "October",
+  "November",
+  "December",
 ];
 
 function renderCalender() {
-	calender.innerHTML = "";
-	const startDate = new Date(month.getFullYear(), month.getMonth(), 1);
+  calender.innerHTML = "";
+  const startDate = new Date(month.getFullYear(), month.getMonth(), 1);
 
-	// If the first date of the month is not a monday, make the startdate the previous monday.
-	if (startDate.getDay() != 1) {
-		startDate.setDate(startDate.getDate() - startDate.getDay() + 1);
-	}
+  // If the first date of the month is not a monday, make the startdate the previous monday.
+  if (startDate.getDay() != 1) {
+    startDate.setDate(startDate.getDate() - startDate.getDay() + 1);
+  }
 
-	for (let i = 0; i < 6; i++) {
-		for (let j = 0; j < 7; j++) {
-			const date = new Date(startDate.getTime());
-			date.setDate(startDate.getDate() + i * 7 + j);
+  for (let i = 0; i < 6; i++) {
+    for (let j = 0; j < 7; j++) {
+      const date = new Date(startDate.getTime());
+      date.setDate(startDate.getDate() + i * 7 + j);
 
-			const day = document.createElement("div");
-			day.classList.add("grid-item");
+      const day = document.createElement("div");
+      day.classList.add("grid-item");
 
-			if (date.getMonth() == month.getMonth()) {
-				day.classList.add("current-month-item");
-			}
+      if (date.getMonth() == month.getMonth()) {
+        day.classList.add("current-month-item");
+      }
 
-			day.addEventListener("click", (e) => selectDate(e));
+      day.addEventListener("click", (e) => selectDate(e));
 
-			if (
-				date.getDate() === selectedDate?.getDate() &&
-				date.getMonth() === selectedDate?.getMonth()
-			) {
-				day.classList.add("highlight-grid-item");
-			}
+      if (
+        date.getDate() === selectedDate?.getDate() &&
+        date.getMonth() === selectedDate?.getMonth()
+      ) {
+        day.classList.add("highlight-grid-item");
+      }
 
-			const dayNr = document.createElement("p");
-			dayNr.innerText = date.getDate();
-			dayNr.classList.add("day-nr");
-			day.appendChild(dayNr);
+      const dayNr = document.createElement("p");
+      dayNr.innerText = date.getDate();
+      dayNr.classList.add("day-nr");
+      day.appendChild(dayNr);
 
-			day.dataset.id = date.getTime();
-			calender.appendChild(day);
-		}
-	}
+      day.dataset.id = date.getTime();
+      calender.appendChild(day);
+    }
+  }
 }
 
 function selectDate(e) {
-	const targetDate = new Date(Number(e.target.dataset.id));
+  const targetDate = new Date(Number(e.target.dataset.id));
 
-	if (
-		targetDate.getDate() === selectedDate?.getDate() &&
-		targetDate.getMonth() === selectedDate?.getMonth()
-	)
-		selectedDate = null;
-	else {
-		selectedDate = targetDate;
-	}
+  if (
+    targetDate.getDate() === selectedDate?.getDate() &&
+    targetDate.getMonth() === selectedDate?.getMonth()
+  )
+    selectedDate = null;
+  else {
+    selectedDate = targetDate;
+  }
 
-	renderCalender();
+  renderCalender();
 }
 
 function getSelectedDate() {
-	return selectedDate;
+  return selectedDate;
 }
 
 export { renderCalender, getSelectedDate };
