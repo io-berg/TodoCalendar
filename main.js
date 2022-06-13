e2841a636858e6219db99ff47de5184d77c5d939
Eva-Li8E <evaliboden@gmail.com>
2022-06-13 11:17:18 +0200

    underrubrik månad dagar

---
+++ main.js
@@ -0,0 +1,18 @@
+const date = new Date();
+
+const months = [
+  "Januari",
+  "Februari",
+  "Mars",
+  "April",
+  "Maj",
+  "Juni",
+  "Juli",
+  "Augusti",
+  "September",
+  "Oktober",
+  "November",
+  "December",
+];
+
+document.querySelector(".månad h1").innerHTML = months[date.getMonth()];
