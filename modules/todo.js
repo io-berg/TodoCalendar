ccb37f805faca6e8cb35b8437b069b78e3fae3ab
Ola Larsson <soaring.jr@hotmail.com>
2022-06-13 15:10:38 +0200

    basic functionality added

--- modules/todo.js
+++ modules/todo.js
@@ -12,6 +12,7 @@ function addTodos() {
   };
 
   todos.push(todo);
+  renderTodoList();
   toggleTodoForm();
 }
 
@@ -28,3 +29,29 @@ const addButton = document.getElementById("addTodo-button");
 addButton.addEventListener("click", toggleTodoForm);
 const createTodoButton = document.getElementById("createTodo-button");
 createTodoButton.addEventListener("click", addTodos);
+
+const todoListContainer = document.getElementById("todo-list-container");
+
+function renderTodoList() {
+  todoListContainer.innerHTML = "";
+  for (const todo of todos) {
+    const todoDiv = document.createElement("div");
+    todoDiv.classList.add("todoListDiv");
+    for (const key of Object.keys(todo)) {
+      const todoTextField = document.createElement("p");
+      if (key == "description") {
+        const todoTextFieldValue = document.createElement("p");
+        todoTextField.innerHTML = `${key}:`;
+        todoTextFieldValue.innerHTML = `${todo[key]}`;
+        todoDiv.appendChild(todoTextField);
+        todoDiv.appendChild(todoTextFieldValue);
+      } else {
+        todoTextField.innerHTML = `${key}: ${todo[key]}`;
+        todoDiv.appendChild(todoTextField);
+      }
+    }
+    todoListContainer.appendChild(todoDiv);
+  }
+}
+
+renderTodoList();
