705e86c2ad40b10f523919f6ddfc0777cccd3aea
Ola Larsson <soaring.jr@hotmail.com>
2022-06-17 10:57:34 +0200

    Changed setDateFact to use current date

--- main.js
+++ main.js
@@ -2,10 +2,12 @@ import {
   getSelectedDate,
   renderCalender,
   renderCalenderMonth,
+  setDateFact,
 } from "./modules/calender.js";
 import { renderTodoList, loadTodosFromLocalStorage } from "./modules/todo.js";
 
 loadTodosFromLocalStorage();
+setDateFact(new Date());
 renderCalenderMonth();
 renderCalender();
 renderTodoList();
