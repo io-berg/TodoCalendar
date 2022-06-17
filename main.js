81f6e1d5fe3290e008156efbf0dccb2233c61655
Isabella Ekdahl <isabellaekdahl@outlook.com>
2022-06-17 12:51:49 +0200

    fixed button

--- main.js
+++ main.js
@@ -4,26 +4,13 @@ import {
   renderCalenderMonth,
 } from "./modules/calender.js";
 import { renderTodoList, loadTodosFromLocalStorage } from "./modules/todo.js";
+import { getRandomUser } from "./modules/headerprofil.js";
 
 loadTodosFromLocalStorage();
 renderCalenderMonth();
 renderCalender();
 renderTodoList();
 
-async function getRandomUser() {
-  const response = await fetch(`https://randomuser.me/api/`);
-  const data = await response.json();
-  const user = data.results[0];
-  console.log(user);
-  displayUser(user);
-}
-function displayUser(user) {
-  const name = document.getElementById("name");
-  const image = document.getElementById("image");
-
-  name.innerText = `${user.name.title} ${user.name.first} ${user.name.last}`;
-  image.setAttribute(`src`, `${user.picture.large}`);
-}
 getRandomUser();
 
-export { getRandomUser, displayUser };
+//export { getRandomUser, };
--- modules/headerprofil.js
+++ modules/headerprofil.js
@@ -1,1 +1,19 @@
+async function getRandomUser() {
+  const response = await fetch(`https://randomuser.me/api/`);
+  const data = await response.json();
+  const user = data.results[0];
+  //console.log(user);
+  displayUser(user);
+}
+function displayUser(user) {
+  const name = document.getElementById("name");
+  const image = document.getElementById("image");
 
+  name.innerText = `${user.name.title} ${user.name.first} ${user.name.last}`;
+  image.setAttribute(`src`, `${user.picture.large}`);
+}
+
+const userBtn = document.getElementById("userBtn");
+userBtn.addEventListener("click", getRandomUser);
+
+export { getRandomUser };
