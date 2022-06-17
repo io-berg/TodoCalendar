1ab75c21ee244d18eeacd97dabfbbd9b1937818d
Eva-Li8E <evaliboden@gmail.com>
2022-06-17 11:19:57 +0200

    header profil

--- main.js
+++ main.js
@@ -9,3 +9,21 @@ loadTodosFromLocalStorage();
 renderCalenderMonth();
 renderCalender();
 renderTodoList();
+
+async function getRandomUser() {
+  const response = await fetch(`https://randomuser.me/api/`);
+  const data = await response.json();
+  const user = data.results[0];
+  console.log(user);
+  displayUser(user);
+}
+function displayUser(user) {
+  const name = document.getElementById("name");
+  const image = document.getElementById("image");
+
+  name.innerText = `${user.name.title} ${user.name.first} ${user.name.last}`;
+  image.setAttribute(`src`, `${user.picture.large}`);
+}
+getRandomUser();
+
+export { getRandomUser, displayUser };
